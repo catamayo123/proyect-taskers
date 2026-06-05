0d94d3487bd4eaed36bc032f187d98c2afe60554
catamayo123 <agnl4003@gmail.com>
2026-06-04 20:51:32 -0400

    Creando el modulo users

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('saludos') // esto es un decorador y dentro de los () va la ruta adicional donde se va a ejecutar la funcion 
   getHello(): string {
     return this.appService.getHello();
   }
---
+++ src/users/controllers/users.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  // aqui se colocan los decoradores de la clases que se vallana a usar
+}
