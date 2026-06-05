71fba448094911edce8192bda6f6650a29e41ce0
catamayo123 <agnl4003@gmail.com>
2026-06-05 11:13:06 -0400

    clase 0 terminada

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UsersModule], // 1. Módulos externos que se importan
-  controllers: [AppController],  // 2. Controladores que pertenecen a este módulo
-  providers: [AppService], // 3. Servicios o proveedores disponibles
 })
-// Explicacion del decorador @Module
-/* 
-  1. imports: []
-    - Aquí se colocan otros módulos que este módulo necesita usar.
-    - Por ejemplo: si quieres usar base de datos, autenticación, o módulos de terceros, los importas aquí.
-    - Actualmente está vacío ([]) porque es la configuración por defecto y aún no se ha importado ningún módulo adicional.
-
-  2. controllers: [AppController]
-    - Aquí se registran los controladores que pertenecen a este módulo.
-    - Los controladores son los encargados de recibir las peticiones HTTP (como GET, POST, etc.) y manejar las rutas.
-    - En este caso, NestJS incluye por defecto el AppController, que es el controlador principal.
-
-  3. providers: [AppService]
-    - Aquí se registran los proveedores (generalmente servicios).
-    - Los providers son clases que contienen la lógica de negocio (por ejemplo, conexiones a base de datos, cálculos, validaciones, etc.).
-    - AppService es el servicio por defecto que NestJS crea junto con el controlador.
-*/
 
 export class AppModule {}
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
+
 
 @Controller('users')
 export class UsersController {
   // aqui se colocan los decoradores de la clases que se vallana a usar
+  constructor(private readonly userService: UsersService) {}
+  
+    @Get('saludos') // esto es un decorador y dentro de los () va la ruta adicional donde se va a ejecutar la funcion 
+    getHello(): string {
+      return this.userService.getHello();
+    }
 }
