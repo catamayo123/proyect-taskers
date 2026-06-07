72b3058f67c9be059a0a105cbaf78fc3c2026ce0
catamayo123 <agnl4003@gmail.com>
2026-06-06 20:11:21 -0400

    intentando instalar docker

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule); // creando una instancia del modulo appModule
   app.setGlobalPrefix('api') // todas las rutas comenzaran con api/ lo que venga
   app.use(morgan('dev')) // mostrar el metodo, ubucacion, estado .... en consola
+
   // Habilita CORS con opciones predeterminadas
   app.enableCors({
-
     origin: true, // Orígenes permitidos [], o en true para este caso pero en produccion poner el arr con los accesos
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Métodos HTTP permitidos
     credentials: true, // Permite que se envíen cookies y encabezados de autorización
-
   }) 
 
   //app.enableCors() // Habilita CORS con opciones predeterminadas
 
   const configService = app.get(ConfigService); // obteneniendo lo que esta en config
   await app.listen( + configService.get('PORT')); // se coloca + para que se convierta en numero 
+  console.log(`La app esta se esta ejecutando en: ${await app.getUrl()}`);
 }
 bootstrap();
