794a888cbf9dc68c018936656e4345af73830eed
catamayo123 <agnl4003@gmail.com>
2026-06-15 19:02:03 -0400

    CRUD de usuario manualmente

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,6 +1,6 @@
 // esto es una clase que va a contener todos los atributos de la calse usuario para validarlos
 
-import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsEnum, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { ROLES } from "src/config/roles";
 
 export class UserDTO {
@@ -30,6 +30,40 @@ export class UserDTO {
     password!: String;
 
     @IsNotEmpty()
+    @IsEnum(ROLES) // enum o constante numerica de tipo rol. ROLE esta ubicado en src/config/roles
+    role!: ROLES;
+}
+
+// clase para poder modificar sin ttener que pasar toda la clase DTO en el servicio, sino solamente el parametro por el cual
+// se desee modificar
+
+export class UserUpdateDTO {
+
+    @IsOptional()
+    @IsString() 
+    first_Name!: String;
+
+    @IsOptional()
+    @IsString()
+    last_Name!: String;
+
+    @IsOptional()
+    @IsNumber()
+    age!: Number;
+
+    @IsOptional()
+    @IsString()
+    email!: String;
+
+    @IsOptional()
+    @IsString()
+    username!: String;
+
+    @IsOptional()
+    @IsString()
+    password!: String;
+
+    @IsOptional()
     @IsEnum(ROLES) // enum o constante numerica de tipo rol. ROLE esta ubicado en src/config/roles
     role!: ROLES;
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { UsersEntity } from '../entites/users.entity';
+import { UserDTO, UserUpdateDTO } from '../dtos/user.dto';
+import { DeleteResult } from 'typeorm/browser';
 
 @Injectable()
 export class UsersService {
@@ -14,7 +16,83 @@ export class UsersService {
     @InjectRepository(UsersEntity) private readonly userRepository: Repository<UsersEntity>
 
   ) { }
-  getHello(): string {
-    return ' Hola Usuario !';
+
+  // CREAR USUARIO que retornara una promesa de tipo UsersEntity que respondera con el mismo objeto que se esta creando
+  /**
+   * async createUser (cuerpo del objeto) return promesa {creacion del usuario y retornar error en caso de que no se cree}
+   */
+  public async createUser(body: UserDTO): Promise<UsersEntity> {
+    try {
+      return await this.userRepository.save(body)
+    } catch (error) {
+      throw new Error('No se creo el Usuario. Error Inesperado')
+    }
+  }
+
+  // LISTAR USUARIO que retorna una promesa con la lista de ususrios 
+  /**
+   * async findUsers 
+   */
+  public async findUsers(): Promise<UsersEntity[]> {
+    try {
+      return await this.userRepository.find()
+    } catch (error) {
+      throw new Error('No encontraron usuarios')
+    }
+  }
+
+  // LISTAR USUARIO POR EL ID 
+  /*
+  retorna una promesas el usuario cuyos ID sean igual al pasado por parametro.
+  el metodo createQueryBuilder() necesita un alias y en este caso es usuario, y es el que permite encadenar metodos,
+  con el podemos acceder a la propiedad where para buscar el ID
+  */
+  /**
+     * async findUsersById 
+     */
+  public async findUsersById(id: string): Promise<UsersEntity> {
+    try {
+      return await this.userRepository.createQueryBuilder('user').where({ id }).getOneOrFail();
+    } catch (error) {
+      throw new Error(`No se creo el Usuario con id: ${id}`)
+    }
+  }
+
+  // MODIFICAR USUARIO POR EL ID 
+  /*
+    Aqui es donde se pone de manifiesto la clase UserUpdateDTO creada en el archivo user.dto.ts
+    retorna una Promise < UpdateResult | undefined > porque despues sera manejada en el controlador
+  */
+  /**
+    * async updateUser 
+  */
+  public async updateUser(body: UserUpdateDTO, id: string): Promise<UpdateResult | undefined> {
+    try {
+      const user: UpdateResult = await this.userRepository.update(id, body)
+
+      if (user.affected === 0) {
+        return undefined; // hay que manejar que se va a responder aqui en el controlador
+      }
+      return user;
+    } catch (error) {
+      throw new Error(`No logro modificar el Usuario con id: ${id}`)
+    }
+  }
+
+  // ELIMINAR USUARIO POR EL ID 
+  /**
+    * async deleteUser 
+  */
+  public async deleteUser(id: string): Promise<DeleteResult | undefined> {
+    try {
+      const user: DeleteResult = await this.userRepository.delete(id)
+
+      if (user.affected === 0) {
+        return undefined; // hay que manejar que se va a responder aqui en el controlador
+      }
+      return user;
+    } catch (error) {
+      throw new Error(`No logro modificar el Usuario con id: ${id}`)
+    }
   }
 }
